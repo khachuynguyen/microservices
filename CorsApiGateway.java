aec854cc447e828aeee0e3b4ab7b71a3a3ac1756
khachuynguyen <118955210+khachuynguyen@users.noreply.github.com>
2023-10-11 16:09:30 +0700

    Create CorsApiGateway.java
    Add CorsConfig To Root Folder( This is apply for ApiGateway)

---
+++ CorsApiGateway.java
@@ -0,0 +1,24 @@
+package apigateway.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+
+@Configuration
+public class CorsConfig{
+
+
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+}
